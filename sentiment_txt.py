5c978c95f96645f8e93ab198250e8c52bc9e2e68
Preston Holmes <ptone@google.com>
2022-03-17 16:53:30 +0000

    add function hello world

--- sentiment_txt.py
+++ sentiment_txt.py
@@ -73,7 +73,14 @@ def sample_analyze_sentiment(text_content):
 
 
 
-
+def hello_world(request):
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello World!'
 
 
 
